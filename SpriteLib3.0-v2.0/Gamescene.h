1ed085fb58967ab549195870bfef84dc2f31b452
Nathan <nathaniel.m.787@gmail.com>
2020-10-12 17:18:00 -0400

    Added SpriteLib3.0-v2.0
    Version from tutorial 3

---
+++ SpriteLib3.0-v2.0/Gamescene.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Scene.h"
+
+class Gamescene : public Scene
+{
+public:
+	Gamescene(std::string name);
+
+	void InitScene(float windowWidth, float windowHeight) override;
+
+	void Update() override;
+
+	//Input overrides
+	void KeyboardHold() override;
+	void KeyboardDown() override;
+	void KeyboardUp() override;
+
+protected:
+
+};
